4c21c1728f0e9f9ea79adc60fb1c35a209a30b7c
Devric <billbox13@gmail.com>
2013-05-21 19:30:48 +1000

    complete calc

--- app/app.js
+++ app/app.js
@@ -31,3 +31,129 @@ Ext.application({
 */
 
 
+
+/**
+ *
+ * Learn
+ * - how to use addBehaviors to add interaction delegations to dom objects
+ * - '#one, #two a, #three div@click' : function(evt,el) {  }
+ */
+
+var cal=function() { 
+    switch(op) 
+    {
+            case "-" :
+                first=parseFloat(first) - parseFloat(second);
+                break;
+            case "*" :
+                first=parseFloat(first) * parseFloat(second);
+                break;
+            case "/" :
+                second=parseFloat(second);
+                if (second !=0)
+                        first=parseFloat(first) / second
+                break;
+            default:
+                first=parseFloat(first) + parseFloat(second);
+                break;
+    }
+
+    op="";
+    if( arguments.length>0 ) op=arguments[0];
+    second="";
+    result.value=first;
+}
+
+var first=""
+ ,  second =""
+ , op =""
+ , result = Ext.getDom('result')
+;
+
+Ext.addBehaviors({ 
+    "input.number@click" : function(e,el) {
+        if ( Ext.isEmpty(op) ) 
+        {
+            if (!(el.value==0 && first==0))
+            {
+                first = first+el.value;
+                result.value=first;
+            }
+            else
+            {
+                if (!(el.value==0 && second==0)) 
+                {
+                     second=second+el.value;
+                     result.value=second;
+                }
+            }
+        }
+    }
+
+  , "input.cmd@click" : function(e,el) {
+        if (el.value=='C')
+        {
+            if (Ext.isEmpty(op))
+            {
+                first="";
+            }
+            else
+            {
+                second="";
+            }
+            result.value="0";
+        }
+        else
+        {
+            cal();
+        }
+    }
+
+  , "input.sign@click" : function(e, el) {
+        if (el.value=".")
+        {
+            if (Ext.isEmpty(op))
+            {
+                if(first.toString().indexOf(".")==-1)
+                {
+                    first=first+".";
+                    result.value=first;
+                }
+                else
+                {
+                    if (second.toString().indexOf(".")==-1)
+                    {
+                        second=second+".";
+                        result.value=second;
+                    }
+                }
+            }
+            else
+            {
+                if (Ext.isEmpty(op))
+                {
+                    first=first*-1;
+                    result.value=first;
+                }
+                else
+                {
+                    second=second*-1;
+                    result.value=second;
+                }
+            }
+        }
+    }
+
+  , "input.op@click" : function(e, el) {
+        if (Ext.isEmpty(op) || Ext.isEmpty(second))
+        {
+            op=el.value;
+            result.value="0";
+        }
+        else
+        {
+            cal(el.value);
+        }
+    }
+
+});
