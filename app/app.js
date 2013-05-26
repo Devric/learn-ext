1d05a2521469bec206b538140764f0b344391013
Devric <billbox13@gmail.com>
2013-05-26 17:33:09 +1000

    simple binding

--- app/app.js
+++ app/app.js
@@ -1,51 +1,9 @@
-/*
-    This file is generated and updated by Sencha Cmd. You can edit this file as
-    needed for your application, but these edits will have to be merged by
-    Sencha Cmd when it performs code generation tasks such as generating new
-    models, controllers or views and when running "sencha app upgrade".
-
-    Ideally changes to this file would be limited and most work would be done
-    in other places (such as Controllers). If Sencha Cmd cannot merge your
-    changes and its generated code, it will produce a "merge conflict" that you
-    will need to resolve manually.
-*/
-
-// DO NOT DELETE - this directive is required for Sencha Cmd packages to work.
-//@require @packageOverrides
-
-/*
-Ext.application({
-    name: 'do',
-
-    views: [
-        'Main',
-        'Viewport'
-    ],
-
-    controllers: [
-        'Main'
-    ],
-
-    autoCreateViewport: true
-});
-*/
-
-
 Ext.onReady(function() { 
-    if(Ext.BLANK_IMAGE_URL.substr(0,4) != "data")
-    {
-        Ext.BLANK_IMAGE_URL="./mages/s.gif"
-    }
+    Ext.fly("selectAll").on("click", function(e, el){
+        var els = Ext.query("input[name=articleId]");
 
-    // create view port
-    Ext.create('Ext.Viewport', {
-        layout:'fit'
-      , items: [
-            {
-              xtype:"panel"
-            , title: "hi"
-            , html: "<h1>yo</h1>"
-            }
-        ]
+        for ( var i=0; i < els.length; i++ ) {
+            els[i].checked=el.checked
+        }
     });
 });
