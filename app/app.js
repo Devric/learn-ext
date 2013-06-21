7b4fe61ff5eb9aa05d8647a4f67c0f0965386788
Devric <billbox13@gmail.com>
2013-06-21 15:00:56 +1000

    complete with a small bug

--- app/app.js
+++ app/app.js
@@ -1,51 +1,27 @@
-/*
-    This file is generated and updated by Sencha Cmd. You can edit this file as
-    needed for your application, but these edits will have to be merged by
-    Sencha Cmd when it performs code generation tasks such as generating new
-    models, controllers or views and when running "sencha app upgrade".
-
-    Ideally changes to this file would be limited and most work would be done
-    in other places (such as Controllers). If Sencha Cmd cannot merge your
-    changes and its generated code, it will produce a "merge conflict" that you
-    will need to resolve manually.
-*/
-
-// DO NOT DELETE - this directive is required for Sencha Cmd packages to work.
-//@require @packageOverrides
-
-/*
-Ext.application({
-    name: 'do',
-
-    views: [
-        'Main',
-        'Viewport'
-    ],
-
-    controllers: [
-        'Main'
-    ],
-
-    autoCreateViewport: true
-});
-*/
-
-
 Ext.onReady(function() { 
-    if(Ext.BLANK_IMAGE_URL.substr(0,4) != "data")
-    {
-        Ext.BLANK_IMAGE_URL="./mages/s.gif"
+    // dom accordion, not app accordion
+
+    var el = Ext.get("MyAccodion")
+        , els=Ext.query("#MyAccodion>div{display!=none}")
+    ;
+
+    for ( var i = 0 ; i < els.length; i++ ) {
+        els[i].setAttribute("style","display:none");
     }
 
-    // create view port
-    Ext.create('Ext.Viewport', {
-        layout:'fit'
-      , items: [
-            {
-              xtype:"panel"
-            , title: "hi"
-            , html: "<h1>yo</h1>"
-            }
-        ]
-    });
+    el.on('click', function(e, el ){
+
+        var hideEl=Ext.query("#MyAccodion>div{display!=none}")
+
+        var showEl=Ext.query("+div", el);
+
+        if (showEl) {
+            showEl[0].setAttribute("style","");
+        }
+
+        if (hideEl) {
+            hideEl[0].setAttribute("style","display:none");
+        }
+        
+    }, this);
 });
