4eb8ff3ed2d878ee0da1eb065f5ed91308763e08
Devric <billbox13@gmail.com>
2013-07-15 17:32:40 +1000

    unfinished example of creating button and actions

--- app/app.js
+++ app/app.js
@@ -1,29 +1,49 @@
+// PAGE 308, using data model, to request response and append it to the container
+
+
 Ext.require('Ext.Panel');
 
 Ext.onReady(function() { 
+    Ext.define('Test', {
+        extend: 'Ext.data.Model'
+        , fields: ['id', 'TestName']
+        , proxy: {
+            type     : 'rest'
+            , format : 'php'
+            , url    : 'test/response'
+        }
+    })
+
+    var test = "";
+
     var panel = Ext.create("Ext.Panel", {
           renderTo: Ext.getBody()
         , layout: "auto"
         , height: 500
         , width: 400
-        , autoLoad : {
-            url: "Component.js"
-            , renderer: "component"
-          }
-        , renderer: "component"
         , tbar: [
-            {text: "add component", scope: panel, handler: function(){
-                panel.loader.load({
-                    url: "Component1.js"
-                    , renderer: "component"
-                })
+            {text: "read", scope: panel, handler: function(){
+                Test.load(1, {
+                    success: function(d,o) {
+                        test = d;
+                        panel.update(o.response.responseText)
+                    }
+                });
             }}
-            , {text: "removeALL", scope: panel, handler: function(){
-                panel.loader.load({
-                    url:"Component1.js"
-                    , renderer:"component"
-                    , removeAll:true
-                })
+            , {text: "create", scope: panel, handler: function(){
+                var t=Ext.ModelManager.create({id:"", TextName:"new"}, "Test");
+                t.save({
+                    success: function(d,o) {
+                        panel.update(o.response.responseText);
+                    }
+                });
+            }}
+            , {text: "update", scope: panel, handler: function(){
+                Test.save({
+                    success: function(d,o) {
+                        panel.update(o.response.responseText);
+                    }
+                });
             }}
         ]
     });
