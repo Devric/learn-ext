cf2377b1c0f6b7e9ef2ac8a41cf5367dea8020e2
Devric <billbox13@gmail.com>
2014-02-21 02:04:40 +1100

    update

--- app/app.js
+++ app/app.js
@@ -1,51 +1,37 @@
-/*
-    This file is generated and updated by Sencha Cmd. You can edit this file as
-    needed for your application, but these edits will have to be merged by
-    Sencha Cmd when it performs code generation tasks such as generating new
-    models, controllers or views and when running "sencha app upgrade".
-
-    Ideally changes to this file would be limited and most work would be done
-    in other places (such as Controllers). If Sencha Cmd cannot merge your
-    changes and its generated code, it will produce a "merge conflict" that you
-    will need to resolve manually.
-*/
-
-// DO NOT DELETE - this directive is required for Sencha Cmd packages to work.
-//@require @packageOverrides
-
-/*
-Ext.application({
-    name: 'do',
-
-    views: [
-        'Main',
-        'Viewport'
-    ],
-
-    controllers: [
-        'Main'
-    ],
-
-    autoCreateViewport: true
-});
-*/
-
-
 Ext.onReady(function() { 
-    if(Ext.BLANK_IMAGE_URL.substr(0,4) != "data")
-    {
-        Ext.BLANK_IMAGE_URL="./mages/s.gif"
+
+    Ext.apply(Ext.data.SortTypes, { 
+        asCountry: function(v) {
+            return v.country
+        }
+    });
+
+    Ext.data.Types.YUAN = {
+        convert: function(v, data) {
+            v = ( v !== undefined && v !== null v !== '' ) ? parseFloat(v) : 0;
+            return Ext.String.format("${0}", Ext.Number.toFixef(v,2));
+        },
+        SortTypes: function(v) {
+            return parseFloat(v.replace(/$/,""),10);
+        }
     }
 
-    // create view port
-    Ext.create('Ext.Viewport', {
-        layout:'fit'
-      , items: [
-            {
-              xtype:"panel"
-            , title: "hi"
-            , html: "<h1>yo</h1>"
-            }
-        ]
-    });
+    var data = [
+          {name: 'one', nationality: { country : 'china', img: '' }, salary : "USD10000" }
+        , {name: 'two', nationality: { country : 'china', img: '' }, salary : "USD60000" }
+        , {name: 'three', nationality: { country : 'australia', img: '' }, salary : "USD90000" }
+        , {name: 'four', nationality: { country : 'japan', img: '' }, salary : "USD210000" }
+        , {name: 'five', nationality: { country : 'usa', img: '' }, salary : "USD100000" }
+    ]
+
+
+    var store1 = Ext.create('Ext.data.Store', function(){
+        fields: [
+              {name: 'name', type: 'string'}
+            , {name: 'nationality', sortTypes: 'asCountry'}
+            , {name: 'salary', type: 'yuan'}
+        ],
+        data : data
+    })
+    
 });
